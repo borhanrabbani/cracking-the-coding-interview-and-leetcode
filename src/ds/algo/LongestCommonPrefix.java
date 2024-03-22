80a2a38c19750169b357b1245d873aabd91e3f60
Borhan Rabbani <borhanuddinse28@gmail.com>
2024-03-22 15:51:31 +0600

    14. Longest common prefix optimized

--- src/ds/algo/LongestCommonPrefix.java
+++ src/ds/algo/LongestCommonPrefix.java
@@ -22,10 +22,26 @@ public class LongestCommonPrefix {
 		return prefix;
 		
 	}
+	
+	public static String longestCommonPrefixOptimized(String[] strs) {
+		Arrays.sort(strs);
+		String s1=strs[0];
+		String s2=strs[strs.length-1];
+		int idx=0;
+		while(idx<s1.length() && idx<s2.length()) {
+			if(s1.charAt(idx)!=s2.charAt(idx)) {
+				break;
+			}
+			idx++;
+		}
+			
+		return s1.substring(0, idx);
+		
+	}
 
 	public static void main(String[] args) {
 		String[] str = {"flower","flow","flight"};
-		System.out.println(longestCommonPrefix(str));
+		System.out.println(longestCommonPrefixOptimized(str));
 	}
 
 }
