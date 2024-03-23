f38be19cf1720898d74c10922c91512b9bf92551
Borhan Rabbani <borhanuddinse28@gmail.com>
2024-03-23 20:25:49 +0600

    205. Isomorphic Strings

--- src/ds/algo/IsomorphicStrings.java
+++ src/ds/algo/IsomorphicStrings.java
@@ -32,6 +32,21 @@ public class IsomorphicStrings {
 		return true;
 
 	}
+	
+	public static boolean isIsomorphicOptimized(String s, String t) {
+		// optimized than hash Map
+        int[] m1= new int[256];
+        int[] m2= new int[256]; 
+        for(int i=0; i<s.length(); i++){
+            if(m1[s.charAt(i)] != m2[t.charAt(i)]){
+                return false;
+            }
+            m1[s.charAt(i)] = i+1;
+            m2[t.charAt(i)] = i+1;
+        }
+
+        return true;
+    }
 
 	public static void main(String[] args) {
 		
