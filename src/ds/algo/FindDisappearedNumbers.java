eb50452d6775a2330e6574d1aac091fff402a351
Borhan Rabbani <borhanuddinse28@gmail.com>
2024-03-28 23:56:44 +0600

    448. Find All Disappeared Numbers in an Array space optimized

--- src/ds/algo/FindDisappearedNumbers.java
+++ src/ds/algo/FindDisappearedNumbers.java
@@ -45,6 +45,26 @@ public class FindDisappearedNumbers {
 		return resultList;
 		
 	}
+	
+	public static List<Integer> findDisappearedNumbersSpaceOptimized(int[] nums) {
+		List<Integer> resultList = new ArrayList<Integer>();
+		
+		for(int i=0; i<nums.length; i++) {
+			int index = Math.abs(nums[i])-1;
+			if(nums[index]>0) {
+				nums[index]=-nums[index];
+			}
+		}
+		
+		for(int i=0; i<nums.length; i++) {
+			if(nums[i]>0) {
+				resultList.add(i+1);
+			}
+		}
+				
+		return resultList;
+		
+	}
 
 	public static void main(String[] args) {
 		int arr[] = {4,3,2,7,8,2,3,1};
